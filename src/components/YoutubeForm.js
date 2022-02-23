48883ae783edf6e310b89ae3eedce648625671d5
Debora Bucci <dbucci@protonmail.com>
2022-02-23 11:24:11 -0300

    add the 'useFormik' hook to read the data from the three inputs, and set their values respectively

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,24 +1,53 @@
 import React from "react";
+import { useFormik } from "formik";
 
 import classes from "./YoutubeForm.module.css";
 
 const YoutubeForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+  });
+
+  console.log("Form values", formik.values);
+
   return (
     <form className={classes.form}>
       <label htmlFor="text" id="name">
         Name
       </label>
-      <input type="text" id="name" name="name" />
+      <input
+        type="text"
+        id="name"
+        name="name"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
 
       <label htmlFor="text" id="email">
         Email
       </label>
-      <input type="text" id="email" name="email" />
+      <input
+        type="text"
+        id="email"
+        name="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
 
       <label htmlFor="text" id="channel">
         Channel
       </label>
-      <input type="text" id="channel" name="channel" />
+      <input
+        type="text"
+        id="channel"
+        name="channel"
+        onChange={formik.handleChange}
+        value={formik.values.channel}
+      />
 
       <button>Submit</button>
     </form>
