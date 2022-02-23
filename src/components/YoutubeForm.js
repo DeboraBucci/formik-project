9d9c73f9b03a6f4feb19880e395e996cbf17763b
Debora Bucci <dbucci@protonmail.com>
2022-02-23 12:13:05 -0300

    add a validation to the 'useFormik' hook, and restructure code

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -4,16 +4,42 @@ import { useFormik } from "formik";
 import classes from "./YoutubeForm.module.css";
 
 const YoutubeForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "",
-    },
+  const initialValues = {
+    name: "",
+    email: "",
+    channel: "",
+  };
 
-    onSubmit: (values) => {
-      console.log("Form data", values);
-    },
+  const onSubmit = (values) => {
+    console.log("Form data", values);
+  };
+
+  const validate = (values) => {
+    let errors = {};
+
+    if (!values.name) {
+      errors.name = "Required";
+    }
+
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!values.channel) {
+      errors.channel = "Required";
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate,
   });
 
   console.log("Form values", formik.values);
